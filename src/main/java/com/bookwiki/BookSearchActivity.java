458914fee5457ab6f87f688fbbc33974fbb48d04
Jeff Taggart <jeff@jetaggart.com>
2015-01-10 17:48:50 -0700

    Add search activity

---
+++ src/main/java/com/bookwiki/BookSearchActivity.java
@@ -0,0 +1,14 @@
+package com.bookwiki;
+
+import android.os.Bundle;
+
+public class BookSearchActivity extends LoginRequiredActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        setContentView(R.layout.book_search_activity);
+
+        super.onCreate(savedInstanceState);
+    }
+
+}
--- src/main/java/com/bookwiki/MyBooksActivity.java
+++ src/main/java/com/bookwiki/MyBooksActivity.java
@@ -1,14 +1,22 @@
 package com.bookwiki;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
 
 public class MyBooksActivity extends LoginRequiredActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        setContentView(R.layout.main);
+        setContentView(R.layout.my_books_activity);
 
         super.onCreate(savedInstanceState);
+    }
+
+    public void searchBooks(View view) {
+        Intent intent = new Intent(this, BookSearchActivity.class);
+        startActivity(intent);
     }
 }
